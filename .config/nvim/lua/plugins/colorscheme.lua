a18885262696db91093feaf161ee18e2f45c9bcd
saltchicken <john.eicher89@gmail.com>
2024-09-24 18:32:47 -0700

    Change color of TODO

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -6,6 +6,13 @@ return {
 		opts = function()
 			return {
 				transparent = true,
+				on_colors = function(colors)
+					-- colors.todo = colors.green500
+					colors.info = colors.green500 -- This somehow affects TODO
+				end,
+				-- on_highlights = function(highlights, colors)
+				-- 	highlights.todo = colors.green500
+				-- end,
 			}
 		end,
 	},
