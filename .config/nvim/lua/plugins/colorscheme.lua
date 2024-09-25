220e7e43a7335c400408177db3ec3732fc90630a
saltchicken <john.eicher89@gmail.com>
2024-09-25 15:30:57 -0700

    Switch back to solarized-osaka

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -3,45 +3,45 @@
 -- local hsl = hslutil.hslToHex
 
 return {
-	{
-		"saltchicken/solarized-matrix",
-		lazy = true,
-		priority = 1000,
-		opts = function()
-			return {
-				transparent = true,
-				style = "night",
-			}
-		end,
-	},
 	-- {
-	-- 	"craftzdog/solarized-osaka.nvim",
+	-- 	"saltchicken/solarized-matrix",
 	-- 	lazy = true,
 	-- 	priority = 1000,
 	-- 	opts = function()
 	-- 		return {
 	-- 			transparent = true,
-	-- 			---@param colors string
-	-- 			on_colors = function(colors)
-	-- 				-- colors.todo = colors.green500
-	-- 				colors.info = colors.green500 -- This somehow affects TODO
-	-- 				-- colors.base0 = colors.violet
-	-- 				-- colors.WarningMsg = { fg = colors.green500, bold = true }
-	-- 				-- colors.Debug = { fg = colors.green500 }
-	--
-	-- 				-- local new_orange = "#00FF00"
-	-- 				local new_orange = colors.cyan700
-	-- 				colors.orange = new_orange
-	-- 				colors.orange100 = new_orange
-	-- 				colors.orange300 = new_orange
-	-- 				colors.orange500 = new_orange
-	-- 				colors.orange700 = new_orange
-	-- 				colors.orange900 = new_orange
-	-- 			end,
-	-- 			-- on_highlights = function(highlights, colors)
-	-- 			-- 	highlights.todo = colors.green500
-	-- 			-- end,
+	-- 			style = "night",
 	-- 		}
 	-- 	end,
 	-- },
+	{
+		"craftzdog/solarized-osaka.nvim",
+		lazy = true,
+		priority = 1000,
+		opts = function()
+			return {
+				transparent = true,
+				---@param colors string
+				on_colors = function(colors)
+					-- colors.todo = colors.green500
+					colors.info = colors.green500 -- This somehow affects TODO
+					-- colors.base0 = colors.violet
+					-- colors.WarningMsg = { fg = colors.green500, bold = true }
+					-- colors.Debug = { fg = colors.green500 }
+
+					-- local new_orange = "#00FF00"
+					local new_orange = colors.cyan700
+					colors.orange = new_orange
+					colors.orange100 = new_orange
+					colors.orange300 = new_orange
+					colors.orange500 = new_orange
+					colors.orange700 = new_orange
+					colors.orange900 = new_orange
+				end,
+				-- on_highlights = function(highlights, colors)
+				-- 	highlights.todo = colors.green500
+				-- end,
+			}
+		end,
+	},
 }
