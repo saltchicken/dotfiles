987952dedd26ff21554358e4e7395d84c4b2f638
saltchicken <john.eicher89@gmail.com>
2024-09-30 18:54:59 -0700

    Fix startup

--- .config/hypr/setup_workspaces.sh
+++ .config/hypr/setup_workspaces.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 hyprctl dispatch exec 'wezterm --config-file "/home/saltchicken/.config/hypr/wezmatrix.lua"'
-hyprctl dispatch exec 'kitty --title "Main" --hold -e "neofetch"'
 # hyprctl dispatch workspace 2 && firefox
 # hyprctl dispatch workspace 3 && discord
 # hyprctl dispatch workspace 3 && youtube-music
 
 sleep 1
 hyprctl dispatch exec 'pkill -SIGUSR1 waybar'
+# hyprctl dispatch cyclenext && hyprctl dispatch bringactivetotop
+# hyprctl dispatch movecursor 2000 400
+# hyprctl dispatch focuswindow "^(kitty)$"
+hyprctl dispatch exec 'kitty --title "Main" --hold -e "tmux"'
