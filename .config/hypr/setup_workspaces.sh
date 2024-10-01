c5e433b4cfcb59c2144d1c92f6c50a5ff6bc6250
saltchicken <john.eicher89@gmail.com>
2024-09-30 22:13:19 -0700

    Fix bug with order

--- .config/hypr/setup_workspaces.sh
+++ .config/hypr/setup_workspaces.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 hyprctl dispatch exec 'wezterm --config-file "/home/saltchicken/.config/hypr/wezmatrix.lua"'
+hyprctl dispatch exec 'kitty --title "Main" --hold -e "tmux"'
 # hyprctl dispatch workspace 2 && firefox
 # hyprctl dispatch workspace 3 && discord
 # hyprctl dispatch workspace 3 && youtube-music
@@ -10,4 +11,3 @@ hyprctl dispatch exec 'pkill -SIGUSR1 waybar'
 # hyprctl dispatch cyclenext && hyprctl dispatch bringactivetotop
 # hyprctl dispatch movecursor 2000 400
 # hyprctl dispatch focuswindow "^(kitty)$"
-hyprctl dispatch exec 'kitty --title "Main" --hold -e "tmux"'
