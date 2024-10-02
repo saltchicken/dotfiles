71185698a751fb2489aba3a0da06e1f3950cd84c
saltchicken <john.eicher89@gmail.com>
2024-10-01 17:48:20 -0700

    Tailwind bugged

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -32,7 +32,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.lang.json" },
 		-- { import = "lazyvim.plugins.extras.lang.markdown" },
 		{ import = "lazyvim.plugins.extras.lang.rust" },
-		{ import = "lazyvim.plugins.extras.lang.tailwind" },
+		-- { import = "lazyvim.plugins.extras.lang.tailwind" },
 		{ import = "lazyvim.plugins.extras.lang.python" },
 		{ import = "lazyvim.plugins.extras.coding.codeium" },
 		-- { import = "lazyvim.plugins.extras.dap.core" },
