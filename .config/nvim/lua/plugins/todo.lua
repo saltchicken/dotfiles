81637c96090f532a68b4dbe696d5d33759039649
saltchicken <john.eicher89@gmail.com>
2024-10-09 20:59:59 -0700

    Update dependencies

--- .config/nvim/lua/plugins/todo.lua
+++ .config/nvim/lua/plugins/todo.lua
@@ -2,10 +2,14 @@ return {
 
 	{
 		"saltchicken/todo-tui",
+		dependencies = {
+			"saltchicken/windows.nvim",
+		},
 		config = function()
 			local todo = require("todo-tui")
 			todo.setup({
 				debug = true,
+				repo_path = "/home/saltchicken/.local/share/keep",
 			})
 		end,
 	},
