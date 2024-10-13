ae1cf1be64be80710cfa0fd7db4a11e61964a514
saltchicken <john.eicher89@gmail.com>
2024-10-13 16:49:33 -0700

    Change which-key set to global

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -217,7 +217,7 @@ return {
 			{
 				"<leader>?",
 				function()
-					require("which-key").show({ global = false })
+					require("which-key").show({ global = true })
 				end,
 				desc = "Buffer Local Keymaps (which-key)",
 			},
