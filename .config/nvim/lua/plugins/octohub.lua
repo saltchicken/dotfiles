b7b581c4d6d611e7833f4c90b653a660805e52e2
saltchicken <john.eicher89@gmail.com>
2024-10-14 13:56:07 -0700

    Add octohub

---
+++ .config/nvim/lua/plugins/octohub.lua
@@ -0,0 +1,33 @@
+return {
+	"2kabhishek/octohub.nvim",
+	cmd = {
+		"OctoRepos",
+		"OctoRepo",
+		"OctoStats",
+		"OctoActivityStats",
+		"OctoContributionStats",
+		"OctoRepoStats",
+		"OctoProfile",
+		"OctoRepoWeb",
+	},
+	keys = {
+		"<leader>goa",
+		"<leader>goc",
+		"<leader>gof",
+		"<leader>gog",
+		"<leader>goh",
+		"<leader>goi",
+		"<leader>goo",
+		"<leader>gop",
+		"<leader>gor",
+		"<leader>gos",
+		"<leader>got",
+		"<leader>gou",
+		"<leader>gow",
+	},
+	dependencies = {
+		"2kabhishek/utils.nvim",
+		"nvim-telescope/telescope.nvim",
+	},
+	opts = {},
+}
