8da7be79ffa80f5d614ab2302f1de70c61765aa6
saltchicken <john.eicher89@gmail.com>
2024-10-14 19:57:48 -0700

    Add remote-sshfs

---
+++ .config/nvim/lua/plugins/ssh.lua
@@ -0,0 +1,11 @@
+return {
+	"nosduco/remote-sshfs.nvim",
+	dependencies = { "nvim-telescope/telescope.nvim" },
+	opts = {
+		-- Refer to the configuration section below
+		-- or leave empty for defaults
+	},
+	-- config = function()
+	-- 	require("telescope").load_extension("remote-sshfs")
+	-- end,
+}
