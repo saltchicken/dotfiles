bf9be3b6ab8efd3f227f400d6d8a383b91452766
saltchicken <john.eicher89@gmail.com>
2025-10-03 21:47:10 -0700

    Toggle blink with Ctrl+q

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,6 +1,32 @@
 return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "nvim-lualine/lualine.nvim", enabled = false },
+  {
+    "saghen/blink.cmp",
+    opts = function(_, opts)
+      local completion_toggle = Snacks.toggle({
+        name = "Completion",
+        get = function()
+          return vim.b.completion
+        end,
+        set = function(state)
+          vim.b.completion = state
+        end,
+      })
+
+      local function toggle_completion()
+        require("blink.cmp").hide()
+        completion_toggle:toggle()
+      end
+
+      vim.keymap.set({ "i", "n" }, "<C-q>", toggle_completion, { desc = "Toggle Completion" })
+      opts.enabled = function()
+        return vim.b.completion
+      end
+
+      return opts
+    end,
+  },
   -- {
   --   "nvim-lualine/lualine.nvim",
   --   config = function(_, opts)
