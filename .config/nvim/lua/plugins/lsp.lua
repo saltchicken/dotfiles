4776d610c73e1725c113a0d168e59aee2f2c4f94
saltchicken <john.eicher89@gmail.com>
2025-10-20 17:26:49 -0700

    Set virtual_lines for LSP

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,3 +1,12 @@
 return {
-  "simrat39/rust-tools.nvim",
+  { "simrat39/rust-tools.nvim" },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      diagnostics = {
+        virtual_text = false, -- disable inline text
+        virtual_lines = true, -- show under the line
+      },
+    },
+  },
 }
