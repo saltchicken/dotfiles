380cdd00941fa7489db5a885113595dd8e1ec4fd
saltchicken <john.eicher89@gmail.com>
2025-10-20 17:30:18 -0700

    Set toggle for virtual lines for LSP

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -4,8 +4,23 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       diagnostics = {
-        virtual_text = false, -- disable inline text
-        virtual_lines = true, -- show under the line
+        virtual_text = false, -- always off
+        virtual_lines = true, -- start with under-line diagnostics
+      },
+    },
+    keys = {
+      {
+        "<leader>uv",
+        function()
+          local cfg = vim.diagnostic.config()
+          local new_state = not cfg.virtual_lines
+          vim.diagnostic.config({
+            virtual_lines = new_state,
+            virtual_text = false, -- keep it off
+          })
+          vim.notify("Virtual lines " .. (new_state and "enabled" or "disabled"))
+        end,
+        desc = "Toggle virtual lines (under-line diagnostics)",
       },
     },
   },
