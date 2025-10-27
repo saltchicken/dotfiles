9a333828bf31ca9ea9359ff95d4c3f8b0158bb4d
saltchicken <john.eicher89@gmail.com>
2025-10-27 12:26:04 -0700

    Comment string for wgsl

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,9 +3,22 @@ require("config.lazy")
 
 vim.opt.laststatus = 0
 
-vim.api.nvim_create_autocmd("TermOpen", {
-  pattern = "*",
+-- vim.api.nvim_create_autocmd("TermOpen", {
+--   pattern = "*",
+--   callback = function()
+--     vim.opt_local.mouse = ""
+--   end,
+-- })
+
+vim.filetype.add({
+  extension = {
+    wgsl = "wgsl",
+  },
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "wgsl",
   callback = function()
-    vim.opt_local.mouse = ""
+    vim.bo.commentstring = "// %s"
   end,
 })
