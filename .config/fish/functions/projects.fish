e03ab0fa6b1b4499da138c39cddbb11fb05110f2
saltchicken <john.eicher89@gmail.com>
2025-10-31 11:25:56 -0700

    Update saltchicken/project-dash runner function

--- .config/fish/functions/projects.fish
+++ .config/fish/functions/projects.fish
@@ -1,9 +1,9 @@
 # Function to select a desktop project, cd into it, and open nvim
 function projects
-    set selected_dir (~/.cargo/bin/project-selector)
+    set selected_dir (~/.cargo/bin/project-dash)
     if test -n "$selected_dir"
         cd "$selected_dir"
-        nvim .
+        nvim
     end
     # If $selected_dir is empty (user pressed 'q'), the function just ends
 end
