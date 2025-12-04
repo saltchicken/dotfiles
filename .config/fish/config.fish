e91f11a719f8f8b396fef02a2bc2bab22008d1c4
saltchicken <john.eicher89@gmail.com>
2025-12-04 06:49:17 -0800

    Add systemd-tui-monitor

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,10 +11,12 @@ bind \co last_out
 bind \cp projects
 bind \cs sqledger
 bind \cn pgnotes
+bind \cy systemd-tui-monitor
 
 bind -M insert \co last_out
 bind -M insert \cp projects
 bind -M insert \cs sqledger
 bind -M insert \cn pgnotes
+bind -M insert \cy systemd-tui-monitor
 
 fzf --fish | source
