73267699fe9c3b85608039932e1b8e8f785cd910
saltchicken <john.eicher89@gmail.com>
2026-01-21 17:51:21 -0800

    Disable Neocodeum

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -1,10 +1,10 @@
 return {
-  {
-    "monkoose/neocodeium",
-    event = "VeryLazy",
-    config = function()
-      local neocodeium = require("neocodeium")
-      neocodeium.setup()
-    end,
-  },
+	--   {
+	--     "monkoose/neocodeium",
+	--     event = "VeryLazy",
+	--     config = function()
+	--       local neocodeium = require("neocodeium")
+	--       neocodeium.setup()
+	--     end,
+	--   },
 }
