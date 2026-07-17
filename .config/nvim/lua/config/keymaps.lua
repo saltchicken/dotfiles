b8b6289e24625e512f7d56c8a6563f7f649a972a
saltchicken <john.eicher89@gmail.com>
2026-07-16 20:11:24 -0700

    Add <leader>tw to get rid of spaces in python files

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 vim.keymap.set("n", "<leader>hp", function()
   Snacks.dashboard()
 end, { desc = "Show Dashboard" })
+
+vim.keymap.set("n", "<leader>tw", function()
+  local view = vim.fn.winsaveview()
+  vim.cmd([[%s/\s\+$//e]])
+  vim.fn.winrestview(view)
+end, { desc = "Trim trailing whitespace" })
