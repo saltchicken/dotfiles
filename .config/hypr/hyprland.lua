9d2a2a90a9d84508011a7b92ba705c108e49f831
saltchicken <john.eicher89@gmail.com>
2026-08-28 14:22:44 -0700

    Updated

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -73,6 +73,10 @@ hl.env("XCURSOR_SIZE", 24)
 
 hl.env("HYPRCURSOR_SIZE", 24)
 
+-- Qt environment
+hl.env("QT_QPA_PLATFORM", "wayland")
+hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
+
 --##################
 
 --## PERMISSIONS ###
