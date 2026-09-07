d0c88000b72bce8c50fa2137bf3f558a43f07f68
saltchicken <john.eicher89@gmail.com>
2026-09-07 12:15:42 -0700

    Add zoxide

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -67,4 +67,8 @@ function gstyle
     set_color normal
 end
 
-set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/saltchicken/.ghcup/bin $PATH # ghcup-env
+zoxide init fish | source
+
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
+set -gx PATH $HOME/.cabal/bin /home/saltchicken/.ghcup/bin $PATH # ghcup-env
+
